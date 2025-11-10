a72f69aab744521a2f033ee075ca4be21e7f53f0
Karol <karol.kraus@abax.com>
2025-11-10 15:30:43 +0100

    init

---
+++ test/src/index.ts
@@ -0,0 +1,50 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "first-mcp",
+  version: "1.0.0",
+  capabilities: {
+    resorces: {},
+    tools: {},
+  },
+});
+
+server.tool(
+  "add-numbers",
+  "Add two numbers",
+  {
+    a: z.number().describe("Frist number"),
+    b: z.number().describe("Frist number"),
+  },
+  ({ a, b }) => {
+    return {
+      content: [{ type: "text", text: `Total is ${a + b}` }],
+    };
+  }
+);
+
+server.tool(
+  "get_github_repos",
+  "Add two numbers",
+  {
+    a: z.number().describe("Frist number"),
+    b: z.number().describe("Frist number"),
+  },
+  ({ a, b }) => {
+    return {
+      content: [{ type: "text", text: `Total is ${a + b}` }],
+    };
+  }
+);
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+}
+
+main().catch((error) => {
+  console.error("error in main!:", error);
+  process.exit(1);
+});
